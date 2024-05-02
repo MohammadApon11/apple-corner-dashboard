777a3c855906abc618b4b093128a9742bf4bfd74
moahammadApon <mohammadapon11.com>
2024-05-02 23:10:04 +0600

    lading page deploy

--- lading-page/src/utils/index.js
+++ lading-page/src/utils/index.js
@@ -1,1 +1,2 @@
-export const adminBaseURL = "http://localhost:5000/api/posts";
+// export const adminBaseURL = "http://localhost:5000/api/posts"; // development url
+export const adminBaseURL = "https://apple-corner-dashboard-backend.vercel.app/api/posts"; // production url
