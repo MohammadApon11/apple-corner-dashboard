b0e3dfda0c3d07fc4845dd786c5c1d08e49ddd69
moahammadApon <mohammadapon11.com>
2024-05-03 04:20:05 +0600

    done push

--- dashboard-frontend/api/redirect.js
+++
@@ -1,5 +0,0 @@
-export default function handler(req, res) {
-    res.writeHead(307, { Location: '/' });
-    res.end();
-  }
-  
--- dashboard-frontend/vite.config.js
+++ dashboard-frontend/vite.config.js
@@ -1,7 +1,7 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import ssr from 'vite-plugin-ssr';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [ssr()],
-});
+  plugins: [react()],
+})
