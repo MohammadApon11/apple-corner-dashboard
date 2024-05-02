384a29f606afce74ff1b00c67c04f6eb3d73090b
moahammadApon <mohammadapon11.com>
2024-05-03 04:03:20 +0600

    baes url add

--- dashboard-frontend/src/main.jsx
+++ dashboard-frontend/src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="https://apple-corner-dashboard.vercel.app">
       <UtilesContextProvider>
         <AuthContextProvider>
           <App />
